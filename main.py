a8cbbcecacdf2597573138d5d3f7067cdc6cf64d
Lior Levy <liorlevy180@icloud.com>
2024-06-17 14:48:32 +0300

    imdb lib works

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-import imdbpy as imdb
+from imdb import IMDb
+
+# create an instance of the IMDb class
+ia = IMDb()
+
+# get a movie
+advTime = ia.get_movie('1305826')
+
+# print the names of the directors of the movie
+print('Directors:')
+for director in advTime['directors']:
+    print(director['name'])
+
+# print the genres of the movie
+print('Genres:')
+for genre in advTime['genres']:
+    print(genre)
+
+# search for a person name
+people = ia.search_person('Mel Gibson')
+for person in people:
+   print(person.personID, person['name'])
